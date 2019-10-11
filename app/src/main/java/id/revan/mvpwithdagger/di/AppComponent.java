944871c30caad110eadce28e08098fd47ad113b1
revando_tdc <revze@outlook.co.id>
2019-10-12 05:55:02 +0700

    Make AppComponent singleton

--- app/src/main/java/id/revan/mvpwithdagger/di/AppComponent.java
+++ app/src/main/java/id/revan/mvpwithdagger/di/AppComponent.java
@@ -1,7 +1,10 @@
 package id.revan.mvpwithdagger.di;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
+@Singleton
 @Component(modules = {ApiModule.class, UIModule.class})
 public interface AppComponent extends AppGraph {
 }
